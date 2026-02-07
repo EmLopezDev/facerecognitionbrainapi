c9829e0b36ed3906f28b522472850bf0edf18a20
Emmanuel <emlopezdev@gmail.com>
2026-02-07 11:55:40 -0500

    updated db credentials to use render db

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const app = express();
 const config = {
     client: "pg",
     connection: {
-        host: "127.0.0.1",
+        host: process.env.DATABASE_HOST,
         port: "5432",
-        user: "postgres",
-        password: "",
-        database: "smart-brain",
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PW,
+        database: process.env.DATABASE_DB,
     },
 };
 
